ec750e14da7e515de456f07449272091dbb3547b
gericass <bonjovi1926@yahoo.co.jp>
2019-04-26 02:10:24 +0900

    add: debug setting

--- app/src/main/java/com/github/inamura_nakamura_lab/timecard/App.kt
+++ app/src/main/java/com/github/inamura_nakamura_lab/timecard/App.kt
@@ -1,10 +1,13 @@
 package com.github.inamura_nakamura_lab.timecard
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import com.github.inamura_nakamura_lab.timecard.di.apiModule
 import com.github.inamura_nakamura_lab.timecard.di.appModule
+import com.jakewharton.threetenabp.AndroidThreeTen
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import timber.log.Timber
 
 class App : Application() {
     override fun onCreate() {
@@ -14,6 +17,11 @@ class App : Application() {
             androidContext(this@App)
             // modules
             modules(appModule, apiModule)
+        }
+        AndroidThreeTen.init(this);
+        if (BuildConfig.DEBUG) {
+            Stetho.initializeWithDefaults(this)
+            Timber.plant(Timber.DebugTree())
         }
     }
 }
