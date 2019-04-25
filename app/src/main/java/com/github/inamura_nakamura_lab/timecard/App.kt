e65eb3ee53f9c03ce0de6c65ee86a5aab0d227e4
gericass <bonjovi1926@yahoo.co.jp>
2019-04-26 00:20:19 +0900

    add: koin module

---
+++ app/src/main/java/com/github/inamura_nakamura_lab/timecard/App.kt
@@ -0,0 +1,18 @@
+package com.github.inamura_nakamura_lab.timecard
+
+import android.app.Application
+import com.github.inamura_nakamura_lab.timecard.di.appModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+
+class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            // Android context
+            androidContext(this@App)
+            // modules
+            modules(appModule)
+        }
+    }
+}
---
+++ app/src/main/java/com/github/inamura_nakamura_lab/timecard/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.github.inamura_nakamura_lab.timecard.di
+
+import org.koin.dsl.module
+
+val appModule = module {
+
+}
