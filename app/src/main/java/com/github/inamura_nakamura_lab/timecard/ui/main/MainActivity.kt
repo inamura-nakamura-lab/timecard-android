a6bbe7a3538f9b3c786899006d6f7d323c594909
gericass <bonjovi1926@yahoo.co.jp>
2019-04-26 02:07:44 +0900

    attach fragment

--- app/src/main/java/com/github/inamura_nakamura_lab/timecard/ui/main/MainActivity.kt
+++ app/src/main/java/com/github/inamura_nakamura_lab/timecard/ui/main/MainActivity.kt
@@ -2,12 +2,20 @@ package com.github.inamura_nakamura_lab.timecard.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.github.inamura_nakamura_lab.timecard.R
+import com.github.inamura_nakamura_lab.timecard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragment_container, MainFragment.newInstance())
+                .commit()
     }
 }
